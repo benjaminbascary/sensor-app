4f3d5d6dda4722e410d532211e53df1b67ea3809
benjaminbascary97 <benjamin.bascary@patagoniansys.com>
2023-11-14 22:19:27 -0300

    Cleans home screen and adds button to test

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@
 import React, {useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  Button,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -26,36 +27,6 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 import {BleManager} from 'react-native-ble-plx';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
@@ -63,18 +34,18 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  useEffect(() => {
-    const manager = new BleManager();
+  const manager = new BleManager();
 
-    /**
-     * Replace first null argument with an array of UUID's of your devices
-     * You will see your device in the console (I think)
-     */
+  const onPressScanButton = () => {
     manager.startDeviceScan(null, null, (error, device) => {
-      console.log('Scanning...');
-      console.log(device);
+      if (error) {
+        console.log(error);
+        return;
+      } else {
+        console.log(device);
+      }
     });
-  }, []);
+  };
 
   return (
     <SafeAreaView style={backgroundStyle}>
@@ -85,25 +56,9 @@ function App(): JSX.Element {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View>
+          <Text style={{alignSelf: 'center'}}>BLE App</Text>
+          <Button onPress={onPressScanButton} title="Press to scan" />
         </View>
       </ScrollView>
     </SafeAreaView>
